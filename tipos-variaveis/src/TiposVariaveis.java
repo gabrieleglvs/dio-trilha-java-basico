5d8dd8470da1ea698336704277c400fe446726c6
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2023-10-14 22:04:59 -0300

    docs(java): informações sobre tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        //VARIÁVEL
+        int numero = 1;
+        numero = 2;
+        System.out.println(numero);
+
+        //CONSTANTE (convensão de nome (caixa alta) e expressão 'final')
+        final double VALOR_DE_PI = 3.14;
     }
 }
