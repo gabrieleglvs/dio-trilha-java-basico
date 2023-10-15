c6b5f46aadcdf1a63e280caaa5b06311647e80f3
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2023-10-15 13:46:09 -0300

    docs(java): operadores relacionais

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,25 +1,25 @@
 public class Operadores {
     public static void main(String[] args) {
 
-    // classe Operadores.java
-    int a, b;
+    //classe Operadores.java
+    //OPERADORES RELACIONAIS
+    int numero1 = 1;
+    int numero2 = 2;
 
-    a = 5;
-    b = 6;
+    if(numero1 > numero2)
+        System.out.print("Numero 1 maior que numero 2");
 
-    /* EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
-    if(a==b)
-    resultado = "verdadeiro";
-    else
-    resultado = "falso";
+    if(numero1 < numero2)
+        System.out.print("Numero 1 menor que numero 2");
 
-    <Expressão Condicional> ? <Caso condição seja true> : <Caso condição seja false>
-    */
+    if(numero1 >= numero2)
+        System.out.print("Numero 1 maior ou igual que numero 2");
 
-    //MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
-    String resultado = (a==b) ? "verdadeiro" : "false";
+    if(numero1 <= numero2)
+        System.out.print("Numero 1 menor ou igual que numero 2");
 
-    System.out.println(resultado);
+    if(numero1 != numero2)
+        System.out.print("Numero 1 é diferente de numero 2");jav
 
     }
 }
