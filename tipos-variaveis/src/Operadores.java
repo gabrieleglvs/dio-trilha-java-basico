2a45268ffae98d7edd386aa6f483f534bca09de5
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2023-10-15 13:55:12 -0300

    docs(java): operadores logicos

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,25 +1,21 @@
 public class Operadores {
     public static void main(String[] args) {
+        // Operadores.java
+        boolean condicao1=true;
 
-    //classe Operadores.java
-    //OPERADORES RELACIONAIS
-    int numero1 = 1;
-    int numero2 = 2;
+        boolean condicao2=false;
 
-    if(numero1 > numero2)
-        System.out.print("Numero 1 maior que numero 2");
+        /* Aqui estamos utilizando o operador lógico E para fazer a união de duas 
+        expressões. 
+        É como se estivesse escrito:
+        "Se Condicao1 e Condicao2 forem verdadeiras, executar código"
+        */
 
-    if(numero1 < numero2)
-        System.out.print("Numero 1 menor que numero 2");
+        if(condicao1 && condicao2)
+            System.out.print("Os dois valores precisam ser verdadeiros");;
 
-    if(numero1 >= numero2)
-        System.out.print("Numero 1 maior ou igual que numero 2");
-
-    if(numero1 <= numero2)
-        System.out.print("Numero 1 menor ou igual que numero 2");
-
-    if(numero1 != numero2)
-        System.out.print("Numero 1 é diferente de numero 2");jav
-
+        //Se condicao1 OU condicao2 for verdadeira, executar código.
+        if(condicao1 || condicao2)
+            System.out.print("Um dos valores precisa ser verdadeiro");
     }
 }
